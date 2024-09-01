905dbe06cab529543fd534bab39ef4c5a1ba96c5
yuya-hagiwara <yuyahagi1990@icloud.com>
2024-09-02 01:20:55 +0900

    プロジェクトを初期化

---
+++ src/test/java/com/tutorial/nameservice/NameserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.nameservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
