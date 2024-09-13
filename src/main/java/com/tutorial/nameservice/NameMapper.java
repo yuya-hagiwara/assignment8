ce6ce4439cd8fdbc3e89e88d17ff11b4a1be2c71
yuya-hagiwara <yuyahagi1990@icloud.com>
2024-09-13 15:38:07 +0900

    メソッドを大文字に変更。

--- src/main/java/com/tutorial/nameservice/NameMapper.java
+++ src/main/java/com/tutorial/nameservice/NameMapper.java
@@ -11,5 +11,5 @@ public interface NameMapper {
 
 
     @Select("SELECT * FROM names WHERE name LIKE CONCAT(#{prefix}, '%')")
-    List<Name> findByNamestartsWith(String prefix);
+    List<Name> findByNameStartsWith(String prefix);
 }
